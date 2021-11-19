b89fdeff19e8417c2388fb38c9bc7b620b398858
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-11-19 14:56:24 -0600

    Made small changes and added notes for my understanding

--- group_by_exercises.sql
+++ group_by_exercises.sql
@@ -1,17 +1,19 @@
+USE employees;
 
 # 2 SHOW TITLES
 SELECT DISTINCT title
 FROM titles
-GROUP BY title;
+GROUP BY title; # DON'T NEED THIS LINE. BUT PRACTICE IS PRACTICE.
 
 
 # 3 GROUP BY START AND END WITH 'E' LAST NAME ONLY
 
 SELECT last_name
 FROM employees
-WHERE last_name LIKE '%E'
+WHERE last_name LIKE '%E' # CAN SHORTEN BY SAYING 'E%E' FOR START AND END WITH
 AND last_name like 'E%'
-GROUP BY last_name;
+GROUP BY last_name
+ORDER BY last_name; # ADD THIS LINE TO ORDER ALPHABETICALLY.
 
 
 # 4 Update your previous query to now find unique combinations of first and last name where the last name starts and ends with 'E'. You should get 846 rows.
@@ -19,7 +21,8 @@ SELECT first_name, last_name
 FROM employees
 WHERE last_name LIKE '%E'
 AND last_name like 'E%'
-GROUP BY first_name, last_name;
+GROUP BY first_name, last_name
+ORDER BY last_name; # ORDER BY LAST NAME TO BE ALPHABETICAL.
 
 
 # 5 Find the unique last names with a 'q' but not 'qu'.
@@ -30,17 +33,17 @@ AND last_name NOT LIKE '%qu%'
 GROUP BY last_name;
 
 # 6 Add a COUNT() to your results and use ORDER BY to make it easier to find employees whose unusual name is shared with others.
-SELECT last_name, COUNT(last_name)
+SELECT COUNT(*), last_name # THAT'S WHY IT WAS FLIPPED, IT WILL DISPLAY OFF ORDER THROUGH SELECT.
 FROM employees
 WHERE last_name LIKE '%q%'
 AND last_name NOT LIKE '%qu%'
 GROUP BY last_name
-ORDER BY COUNT(last_name);
-# prints what is needed, but in same order as example. count displays on right and names display on left.
+ORDER BY COUNT(*) DESC;
+# prints what is needed, but NOT in same order as example. count displays on right and names display on left.
 
 # 7 Update your query for 'Irena', 'Vidya', or 'Maya'. Use count(*) and GROUP BY to find the number of employees for each gender with those names.
 
-SELECT gender, COUNT(*)
+SELECT COUNT(*), gender # FLIPPED HERE NOW DISPLAYS CORRECTLY AS IN EXAMPLE
 FROM employees
 WHERE first_name IN ('Irena', 'Vidya', 'Maya')
 GROUP BY gender
