2e8cff39905dfc185463e1cc8bd899247961eb1f
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-11-19 09:38:36 -0600

    Changed question numbers to be correct

--- limit_exercises.sql
+++ limit_exercises.sql
@@ -1,17 +1,17 @@
 
-# 1 below
+# 2 below
 SELECT DISTINCT last_name
 FROM employees
 ORDER BY last_name DESC
 LIMIT 10;
 
-# 2 below
+# 3 below
 SELECT emp_no
 FROM salaries
 ORDER BY salary DESC
 LIMIT 5;
 
-# 3 below
+# 4 below
 SELECT emp_no
 FROM salaries
 ORDER BY salary DESC
