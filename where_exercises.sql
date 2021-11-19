cf1e6f47a5c11ef7a96e4081087ff104b14fabcf
Kapena Magallan <kapena.a.m.magallan@gmail.com>
2021-11-19 09:21:28 -0600

    numbered wrong, now should be correct

--- where_exercises.sql
+++ where_exercises.sql
@@ -13,18 +13,18 @@ WHERE last_name LIKE ('%q%'); #The double % with q in between is to show if that
 
 
 # --------------------------- OPTION 2 -------------------- #
-SELECT * # 1
+SELECT * # 1 & 2
 FROM employees
 WHERE gender = 'M' AND (first_name = 'Irena'
 OR first_name = 'Vidya'
 OR first_name = 'Maya');
 
-SELECT * # 2
+SELECT * # 3
 FROM employees
 WHERE last_name LIKE ('%E')
 OR last_name LIKE ('E%');
 
-SELECT * # 3 & 4
+SELECT * # 4
 FROM employees
 WHERE last_name LIKE ('%E')
 AND last_name LIKE ('E%');
